ac589700e2a50b600ab6f448677e329735455c95
Sergio Di Fiore <difiores@outlook.com>
2021-03-03 17:10:41 -0300

    Add project files.

---
+++ SelectionSort/Classifica.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    public static class Classifica
+    {
+        public static void Ascende(int dimensao, double[] sortArray)
+        {
+            var startTime = DateTime.Now;
+
+            for (int i = dimensao - 1; i > 0; i--)
+            {
+                var maior = sortArray[0];
+                int index = 0;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (sortArray[j] > maior)
+                    {
+                        maior = sortArray[j];
+                        index = j;
+                    }
+                }
+
+                var calice = sortArray[i];
+                sortArray[i] = maior;
+                sortArray[index] = calice;
+            }
+
+            var totalTime = DateTime.Now - startTime;
+
+            PrintArray(sortArray, totalTime);
+        }
+
+        public static void Descende(int dimensao, double[] sortArray)
+        {
+            var startTime = DateTime.Now;
+
+            for (int i = dimensao - 1; i > 0; i--)
+            {
+                var maior = sortArray[0];
+                int index = 0;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (sortArray[j] < maior)
+                    {
+                        maior = sortArray[j];
+                        index = j;
+                    }
+                }
+
+                var calice = sortArray[i];
+                sortArray[i] = maior;
+                sortArray[index] = calice;
+            }
+
+            var totalTime = DateTime.Now - startTime;
+
+            PrintArray(sortArray, totalTime);
+        }
+
+        private static void PrintArray(double[] sortArray, TimeSpan totalTime)
+        {
+            foreach (var item in sortArray)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+            Console.WriteLine($"Tempo de processamento: {totalTime}");
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Quantos dados deseja classificar?");
+            Console.WriteLine("ENTER para 100");
+            var ehNumero = int.TryParse(Console.ReadLine(), out int dimensao);
+
+            if (!ehNumero)
+                dimensao = 100;
+
+            string opcao = "0";
+
+            while (opcao != "1" && opcao != "2")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Escolha a opção de classificação:");
+                Console.WriteLine("[1] Ascendente    [2] Descendente");
+                opcao = Console.ReadKey().KeyChar.ToString();
+            }
+
+            Console.Clear();
+
+            double[] sortArray = new double[dimensao];
+
+            for (int i = 0; i < dimensao; i++)
+            {
+                ehNumero = false;
+
+                while (!ehNumero)
+                {
+                    Console.WriteLine($"Digite o valor do {i + 1}º elemento:");
+                    ehNumero = double.TryParse(Console.ReadLine(), out sortArray[i]);
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("** Início do processamento - Aguarde **");
+
+            if (opcao == "1")
+                Classifica.Ascende(dimensao, sortArray);
+            else
+                Classifica.Descende(dimensao, sortArray);
+
+            
+        }
+    }
+}
